3002d2dff5f895eeb6e20037d59a9fc5f3619d48
ssantoscoder <ssantoscoder0@gmail.com>
2023-06-16 21:51:11 -0600

    codigo para crear los token completo

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,35 @@
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js"
+import { createAccessToken } from "../libs/jwt.js";
+
 
 export const register = async (req, res) => {
 
     const { email, password, username } = req.body;
     try {
+
+        const passwordHash = await bcrypt.hash(password, 10)
         const newUser = new User({
             username,
             email,
-            password
+            password: passwordHash,
+        });
+        const userSaved = await newUser.save();
+        const token = await createAccessToken({ id: userSaved._id });
+        res.cookie("token", token);
+        res.json({
+            message: "User created successfully",
+        });
+        
+        res.json({
+            id: userSaved.id,
+            username: userSaved.email,
+            email: userSaved.email,
+            createdAt: userSaved.createdAt,
+            updatedAt: userSaved.updatedAt,
         })
-        const userSaved = await newUser.save()
-        res.json(userSaved)
     } catch (error) {
-        console.log(error);
+        res.status(500).json({ message: error.message });
     }
 };
 export const login = (req, res) => res.send("login");
