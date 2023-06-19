30e24947f23302dde82726b43a4432bfb0e5df4c
ssantoscoder <ssantoscoder0@gmail.com>
2023-06-19 12:31:11 -0600

    desarrollo del backend completo

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from "express";
 import morgan from "morgan";
 import authRoutes from "./routes/auth.routes.js"
+import taskRoutes from "./routes/tasks.routes.js"
 
 const app = express();
+
 app.use(morgan('dev'));
 app.use(express.json());
+
 app.use('/api', authRoutes);
+app.use('/api', taskRoutes);
+
 export default app;
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -32,4 +32,50 @@ export const register = async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 };
-export const login = (req, res) => res.send("login");
+
+export const login = async (req, res) => {
+
+    const { email, password } = req.body;
+    try {
+
+        const userFound = await User.findOne({ email });
+        if (!userFound) return res.status(400).json({ message: "User not found" });
+
+
+        const isMatch = await bcrypt.compare(password, userFound.password);
+        if (!isMatch) return res.status(400).json({ message: "Incorrect password" });
+
+        const token = await createAccessToken({ id: userFound._id });
+        res.cookie("token", token);
+        res.json({
+            id: userFound.id,
+            username: userFound.email,
+            email: userFound.email,
+            createdAt: userFound.createdAt,
+            updatedAt: userFound.updatedAt,
+        })
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export const logout = (req, res) => {
+    res.cookie("token", "", {
+        expires: new Date(200),
+    });
+    return res.sendStatus(200);
+}
+
+export const profile = async(req, res) => {
+    const userFound = await User.findById(req.user.id)
+
+    if (!userFound) return res.status(400).json({ message: "User not found"});
+
+    return res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        createdAt: userFound.createdAt,
+        updatedAt: userFound.updatedAt,
+    });
+}
